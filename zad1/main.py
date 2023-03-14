47a92e807b222d169de1648bc03e814449e53674
Michal Banasiak <bananiwo@gmail.com>
2023-03-14 20:25:58 +0100

    fix user input

--- zad1/main.py
+++ zad1/main.py
@@ -1,18 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def constant_noise():
-    # Set the sampling rate and duration of the signal
+def get_input():
     time_start = int(input('Podaj czas początkowy:'))
-    time_to_end = int(input('Podaj czas trwania sygnału:'))
-    duration = time_start + time_to_end
+    duration = int(input('Podaj czas trwania sygnału:'))
+    time_to_end = time_start + duration
     amplitude = float(input('Podaj amplitude sygnału:'))
     sampling_rate = int(input('Podaj częstotliwość próbkowania:'))
 
-    random_Y = np.random.uniform(-amplitude/2, amplitude/2, sampling_rate * duration)
+    return time_start, time_to_end, amplitude, sampling_rate
+
+
+def constant_noise():
+    # Set the sampling rate and duration of the signal
+    time_start, time_to_end, amplitude, sampling_rate = get_input()
+    random_Y = np.random.uniform(-amplitude/2, amplitude/2, sampling_rate * time_to_end)
 
     # Create a time axis for the signal
-    t = np.linspace(time_start, duration, sampling_rate*duration)
+    t = np.linspace(time_start, time_to_end, sampling_rate * time_to_end)
 
 
     # Plot the signal
