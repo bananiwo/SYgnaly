94c9440906485beef5dc5e1aa3257b1dd49f6d1b
Michal Banasiak <bananiwo@gmail.com>
2023-03-14 20:18:42 +0100

    constant noise

--- zad1/main.py
+++ zad1/main.py
@@ -6,27 +6,22 @@ def constant_noise():
     time_start = int(input('Podaj czas początkowy:'))
     time_to_end = int(input('Podaj czas trwania sygnału:'))
     duration = time_start + time_to_end
+    amplitude = float(input('Podaj amplitude sygnału:'))
+    sampling_rate = int(input('Podaj częstotliwość próbkowania:'))
 
-    sampling_rate = 1000  # samples per second
+    random_Y = np.random.uniform(-amplitude/2, amplitude/2, sampling_rate * duration)
 
     # Create a time axis for the signal
     t = np.linspace(time_start, duration, sampling_rate*duration)
 
-    # Generate the constant signal of amplitude 2
-    constant_signal = 2*np.ones_like(t)
 
     # Plot the signal
-    plt.plot(t, constant_signal)
+    plt.plot(t, random_Y)
     plt.xlabel('Time (s)')
     plt.ylabel('Amplitude')
-    plt.title('Constant signal of amplitude 2')
+    title_str = 'Constant noise of amplitude ' + str(amplitude)
+    plt.title(title_str)
     plt.show()
 
 constant_noise()
-
-# Prompt the user to enter a value and store it in a variable
-value = input("Enter a value: ")
-
-# Print the entered value
-print("You entered:", value)
 
