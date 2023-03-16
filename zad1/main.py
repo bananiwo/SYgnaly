e9d3f721f93c4542ee1242c5e7e9edd598760a12
Michal Banasiak <bananiwo@gmail.com>
2023-03-16 19:45:42 +0100

    rectangular symmetrical signal

--- zad1/main.py
+++ zad1/main.py
@@ -44,37 +44,47 @@ def jump():
 
     draw_graph("Constant noise", time_start, time_to_end, amplitude, nr_of_samplings, values_y)
 
-# def rectangle_signal():
-#     # time_start, time_to_end, amplitude, sampling_rate = get_input()
-#     time_start, time_to_end, amplitude, sampling_rate = 0, 10, 10, 10
-#     # basic_period = float(input('Podaj okres podstawowy sygnału:'))
-#     # fill_value = float(input('Podaj współczynnik wypełnienia sygnału:'))
-#     basic_period = 20
-#     fill_value = 0.5
-#
-#     nr_of_samplings = sampling_rate * (time_to_end - time_start)
-#     values_y = np.zeros(nr_of_samplings)
-#
-#     # k_array = np.zeros(time_to_end - time_start)
-#     # for x in range(0, time_to_end - time_start):
-#     #     k_array[x] = time_start + x
-#
-#     s = basic_period / sampling_rate
+def rectangular_signal(): #6
+    # time_start, time_to_end, amplitude, sampling_rate = get_input()
+    time_start, time_to_end, amplitude, sampling_rate = 0, 10, 10, 5
+    # basic_period = float(input('Podaj okres podstawowy sygnału:'))
+    # fill_value = float(input('Podaj współczynnik wypełnienia sygnału:'))
+    basic_period = 10
+    fill_value = 0.5
 
-    # for x in range(time_start * sampling_rate, time_to_end * sampling_rate, basic_period):
-    #     values_y[x] = amplitude
+    #sampling rate / basic_period = 5
 
-        # for y in range(time_start, time_to_end):
-        #     for x in range(0, sampling_rate):
-        #         t = y * sampling_rate + x
-        #         if t < y * basic_period + time_start * sampling_rate:
-        #             values_y[t] = amplitude
-        #         elif t < fill_value * basic_period + y * basic_period + time_start * sampling_rate:
-        #             values_y[t] = amplitude
-        #         else:
-        #             values_y[t] = 0
+    nr_of_samplings = sampling_rate * (time_to_end - time_start)
+    values_y = np.zeros(nr_of_samplings)
 
-        # draw_graph("Rectangle signal", time_start, time_to_end, amplitude, nr_of_samplings, values_y)
+    for x in range(time_start * sampling_rate, time_to_end * sampling_rate, basic_period):
+        for i in range(int(basic_period * fill_value)):
+            values_y[x + i] = amplitude
+
+    draw_graph("Rectangle signal", time_start, time_to_end, amplitude, nr_of_samplings, values_y)
+
+def rectangular_symmetrical_signal(): #7
+    # time_start, time_to_end, amplitude, sampling_rate = get_input()
+    time_start, time_to_end, amplitude, sampling_rate = 0, 10, 10, 1000
+    # basic_period = float(input('Podaj okres podstawowy sygnału:'))
+    # fill_value = float(input('Podaj współczynnik wypełnienia sygnału:'))
+    basic_period = 2
+    fill_value = 0.5
+
+    nr_of_samplings = sampling_rate * (time_to_end - time_start)
+    values_y = np.zeros(nr_of_samplings)
+    for x in range(nr_of_samplings):
+        values_y[x] = -amplitude
+
+    for x in range(time_start, time_to_end, basic_period):
+        for i in range(int(basic_period * fill_value * sampling_rate)):
+            values_y[x * sampling_rate + i] = amplitude
+
+    # for x in range(time_start * sampling_rate, time_to_end * sampling_rate, basic_period * sampling_rate):
+    #     for i in range(int(basic_period * fill_value * sampling_rate)):
+    #         values_y[x + i] = amplitude
+
+    draw_graph("Rectangular symmetrical signal", time_start, time_to_end, amplitude, nr_of_samplings, values_y)
 
 def jump_signal(): #9
     time_start, time_to_end, amplitude, sampling_rate = get_input()
@@ -154,5 +164,6 @@ def gaussian_noise():
 # constant_noise()
 # sinus_half_straight_signal()
 # sinus_double_half_straight_signal()
-# rectangle_signal()
-jump_signal()
+# rectangular_signal()
+rectangular_symmetrical_signal()
+# jump_signal()
